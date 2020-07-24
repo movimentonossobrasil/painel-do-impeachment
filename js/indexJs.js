2afec18b9af992e91891a6128cc542f4de0e89f3
movimentonossobrasil <68712487+movimentonossobrasil@users.noreply.github.com>
2020-07-23 23:10:23 -0300

    Update indexJs.js

--- js/indexJs.js
+++ js/indexJs.js
@@ -1,3 +1,12 @@
+window.onload = () => {
+  'use strict';
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
+  }
+}
+
 function loadScore(element) {
   var text = element.innerText.replace("ã", "a");
   location.href = "selectedIndex.html?selected=" + text;
