b2cdab2b8e573e98d194ced12226e75275fe8b57
movimentonossobrasil <68712487+movimentonossobrasil@users.noreply.github.com>
2020-08-12 22:52:01 -0300

    Update indexJs.js

--- js/indexJs.js
+++ js/indexJs.js
@@ -2,8 +2,33 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw1.js');
+
+    navigator.serviceWorker.register('./sw1.js').then(reg => {
+      reg.addEventListener('updatefound', () => {
+        // A wild service worker has appeared in reg.installing!
+        newWorker = reg.installing;
+
+        newWorker.addEventListener('statechange', () => {
+          // Has network.state changed?
+          switch (newWorker.state) {
+            case 'installed':
+              if (navigator.serviceWorker.controller) {
+                // new update available
+                showUpdateBar();
+              }
+              // No update available
+              break;
+          }
+        });
+      });
+    });
+
+    let refreshing;
+    navigator.serviceWorker.addEventListener('controllerchange', function () {
+      if (refreshing) return;
+      window.location.reload();
+      refreshing = true;
+    });
   }
 }
 
